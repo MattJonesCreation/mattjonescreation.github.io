b5c7bd187ba2f94f3245c4fbffb2b24df120a41d
Matt Jones <mjones@softwaredesignsolutions.com>
2019-03-30 16:27:53 -0400

    Updating about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -31,14 +31,18 @@ class AboutPage extends Component {
                                     actively working as a Full Stack Developer and pride myself on providing real-world solutions to complex technical problems.
                                 </p>
                                 <p>
-                                    I've been working as a Software Engineer for the past 3.5 years. Last year I was
-                                    promoted to team lead at Software Design Solutions. Although not much has changed, I still am 
-                                    actively working as a Full Stack Developer and pride myself on providing real-world solutions to complex technical problems.
+                                    I graduated from Xavier University with degrees in Mathematics and Computer Science. 
+                                    At Xavier University, I graduated as Magna Cum Laude while obtaining a 4.0 in Computer Science 
+                                    related majors and making Deans list every semester. I pride myself on always bringing out the 
+                                    best in my colleagues and I am able to do this because of the impact sports have had on my life.
                                 </p>
                                 <p>
-                                    I've been working as a Software Engineer for the past 3.5 years. Last year I was
-                                    promoted to team lead at Software Design Solutions. Although not much has changed, I still am 
-                                    actively working as a Full Stack Developer and pride myself on providing real-world solutions to complex technical problems.
+                                    Initially, I chose to go to Xavier University as an undecided major to play baseball at a 
+                                    collegiate level. After a couple of injuries I had to give up baseball but the lessons I 
+                                    learned have impacted my life immensely. I learned skills that emphasized teamwork, leadership, 
+                                    and time management. All of these help me to work well with others and being a great problem
+                                    solver. My senior year I was given The Richard J. Wehrmeyer Pi Mu Epsilon Award for
+                                    a Mathematics major exhibiting excellence in problem solving.
                                 </p>
                             </Grid.Column>                                            
                         </Grid.Row>

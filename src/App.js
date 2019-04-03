3c56c0628b8e639237ddbbd216575fd366921d36
Matt Jones <mjones@softwaredesignsolutions.com>
2019-04-02 20:29:42 -0400

    Shouldn't have deleted App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ScreensRoot from './screens/Root';
+import './App.css';
 
-const App = () => ( <ScreensRoot /> );
+const App = () => (
+    <ScreensRoot />
+);
 
 export default App;

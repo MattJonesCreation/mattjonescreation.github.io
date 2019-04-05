8db79fcbd3128388440aa225405bbbb1f1e38b14
Matt Jones <mjones@softwaredesignsolutions.com>
2019-04-05 14:08:29 -0400

    Adding footer with copyright information

--- src/components/HomepageLayout.js
+++ src/components/HomepageLayout.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { NavLink, Link } from "react-router-dom";
-import { Button, Container, Grid, Header, Icon, List, Menu, Responsive, Segment, Sidebar, Visibility } from 'semantic-ui-react';
+import { Button, Container, Grid, Header, Icon, Menu, Responsive, Segment, Sidebar, Visibility } from 'semantic-ui-react';
 import logo from '../images/favicons/favicon.ico';
 
 // Heads up!
@@ -147,42 +147,23 @@ ResponsiveContainer.propTypes = {
 const HomepageLayout = ({ children }) => (
     <ResponsiveContainer>
         {children}
+
         {/* Footer */}
-        <Segment inverted vertical style={{ padding: '5em 0em' }}>
+        <Segment inverted vertical style={{ padding: '2em 0em 1em 0em' }}>
             <Container>
                 <Grid divided inverted stackable>
                     <Grid.Row>
-                        <Grid.Column width={3}>
-                            <Header inverted as='h4' content='About' />
-                            <List link inverted>
-                                <List.Item as='a'>Sitemap</List.Item>
-                                <List.Item as='a'>Contact Us</List.Item>
-                                <List.Item as='a'>Religious Ceremonies</List.Item>
-                                <List.Item as='a'>Gazebo Plans</List.Item>
-                            </List>
-                        </Grid.Column>
-                        <Grid.Column width={3}>
-                            <Header inverted as='h4' content='Services' />
-                            <List link inverted>
-                                <List.Item as='a'>Banana Pre-Order</List.Item>
-                                <List.Item as='a'>DNA FAQ</List.Item>
-                                <List.Item as='a'>How To Access</List.Item>
-                                <List.Item as='a'>Favorite X-Men</List.Item>
-                            </List>
-                        </Grid.Column>
-                        <Grid.Column width={7}>
-                            <Header as='h4' inverted>
-                                Footer Header
-                            </Header>
+                        <Grid.Column width={13}>
                             <p>
-                                Extra space for a call to action inside the footer that could help re-engage users.
-                            </p>
+                                <a href="https://github.com/MattJonesCreation/mattjonescreation.github.io/blob/source/LICENSE">
+                                    All content is copyright © {new Date().getFullYear()} Matt Jones, any trademarks are property of their respective owners
+                                </a>
+                            </p>                          
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
             </Container>
         </Segment>
-        
     </ResponsiveContainer>
 );
 

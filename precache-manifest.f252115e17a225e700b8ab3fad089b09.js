e0fec1100454c826a14eee268e526020f194f0cf
Matt Jones <mjones@softwaredesignsolutions.com>
2019-04-06 16:39:45 -0400

    Updates

---
+++ precache-manifest.f252115e17a225e700b8ab3fad089b09.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c365c411e50e1f6377504ec1d2a2b402",
+    "url": "/static/media/profile.c365c411.JPG"
+  },
+  {
+    "revision": "4159e3df1318d3674fd46cb345d08174",
+    "url": "/static/media/loan-calculator-background.4159e3df.jpg"
+  },
+  {
+    "revision": "716ce2e8a130d9745b0746aa17338132",
+    "url": "/static/media/favicon.716ce2e8.ico"
+  },
+  {
+    "revision": "b2d11483297460557d97c25d5f65bde0",
+    "url": "/static/media/daily-prayer-background.b2d11483.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b1b26e29e8ab8dd7ffba",
+    "url": "/static/js/main.72ca61b8.chunk.js"
+  },
+  {
+    "revision": "dbf8339e55fe6896d88e",
+    "url": "/static/js/2.41bf9d45.chunk.js"
+  },
+  {
+    "revision": "b1b26e29e8ab8dd7ffba",
+    "url": "/static/css/main.d313082b.chunk.css"
+  },
+  {
+    "revision": "7672bd95bfb69a10bb780349ce6efec8",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.138213ec279786b05c41d691e691e9e5.js"
+  "/precache-manifest.f252115e17a225e700b8ab3fad089b09.js"
 );
 
 workbox.clientsClaim();

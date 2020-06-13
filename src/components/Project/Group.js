edac6d8af888b1df7e8a0c17c3dcf0fb119325cb
Matthew Jones <MJones@sessionm.lan>
2020-06-13 08:08:12 -0400

    Adding rustic santa

--- src/components/Project/Group.js
+++ src/components/Project/Group.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, Image } from 'semantic-ui-react';
 import dailyPrayerImg from '../../images/daily-prayer-background.jpg';
 import loanCalculatorImg from '../../images/loan-calculator-background.jpg';
+import rusticSantaImg from '../../images/rustic-santa-background.jpg';
 
 const projects = [
     {
@@ -17,6 +18,13 @@ const projects = [
         href: "https://himattjones.com/financial-react", 
         meta: "March 19, 2019",
         image: <Image src={loanCalculatorImg} />
+    }, 
+    {
+        header: "Rustic Santa",
+        description: "Personal website for the rustic santa.",
+        href: "https://rusticsanta.github.io",
+        meta: "June 12, 2020",
+        image: <Image src={rusticSantaImg} />
     }
 ];
 

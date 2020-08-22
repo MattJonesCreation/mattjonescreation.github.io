fc0f71d640bdc1c59021716fea092cdab3487482
Matthew Jones <MJones@sessionm.lan>
2020-08-22 15:04:13 -0400

    Updating about page

--- src/screens/About/About.js
+++ src/screens/About/About.js
@@ -7,7 +7,8 @@ import './About.css';
 
 const experienceHeaders = ["Company", "Period", "Position"];
 const experienceRows = [
-    ["Software Design Solutions", "May 2016 - Present", "Software Engineer"],
+    ["SessionM, A Mastercard Company", "April 2020 - now", "Software Engineer II"],
+    ["Software Design Solutions", "May 2016 - April 2020", "Software Engineer"],
     ["Epsilon", "July 2015 - May 2016", "Software Developer"]
 ];
 const educationHeaders = ["Degree", "University"];
@@ -40,23 +41,15 @@ const ScreensAbout = () => (
                     </Grid.Column>   
                     <Grid.Column width={8} className="About-intro">
                         <p>
-                            I've been working as a Software Engineer for the past 3.5 years. Last year I was
-                            promoted to team lead at Software Design Solutions. Although not much has changed, I still am 
-                            actively working as a Full Stack Developer and pride myself on providing real-world solutions to complex technical problems.
+                        I've been working as a Software Engineer for the past 5 years. I spent 4 of those years as a consultant 
+                        for Software Design Solutions where I took on a multitude of clients solving complex IoT problems. 
+                        Recently, I started a role as a Software Engineer at SessionM, a Mastercard company.
                         </p>
                         <p>
-                            I graduated from Xavier University with degrees in Mathematics and Computer Science. 
-                            At Xavier University, I graduated as Magna Cum Laude while obtaining a 4.0 in Computer Science 
-                            related classes and making Deans list every semester. I pride myself on always bringing out the 
-                            best in my colleagues and I am able to do this because of the impact sports have had on my life.
-                        </p>
-                        <p>
-                            Initially, I chose to go to Xavier University as an undecided major to play baseball at a 
-                            collegiate level. After a couple of injuries I had to give up baseball but the lessons I 
-                            learned have impacted my life immensely. I learned skills that emphasized teamwork, leadership, 
-                            and time management. All of these help me to work well with others and being a great problem
-                            solver. My senior year I was given The Richard J. Wehrmeyer Pi Mu Epsilon Award for
-                            a Mathematics major exhibiting excellence in problem solving.
+                        I did my undergraduate work at Xavier University, where I double majored in Mathematics and Computer Science. 
+                        At Xavier University, I graduated as Magna Cum Laude while obtaining a 4.0 in Computer Science related majors 
+                        and making Deans list every semester. I pride myself on always bringing out the best in my colleagues and 
+                        I am able to do this because of the impact sports have had on my life.
                         </p>
                     </Grid.Column>                                            
                 </Grid.Row>
